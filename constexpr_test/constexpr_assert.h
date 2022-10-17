cdb08b509d3a604891f4e0cadcf807cc2ccd953b
_ <>
2022-10-17 11:44:36 +0000

    Changes

--- constexpr_test/constexpr_assert.h
+++ constexpr_test/constexpr_assert.h
@@ -9,10 +9,23 @@
 #define C_ASSERT_TRUE(actual_value) C_ASSERT_IMPL(actual_value, true, ==)
 #define C_ASSERT_FALSE(actual_value) C_ASSERT_IMPL(actual_value, false, ==)
 
+namespace constexpr_test {
+    template<int n>
+    struct Str {
+	char data[n];
+	constexpr Str(char const (&s)[n]): data{} { for(int i=0; s[i]; ++i) data[i] = s[i]; }
+    };
+
+    template<auto n>
+    Str(char const (&)[n]) -> Str<n>;
+
+    template<auto>
+    constexpr void fail() { throw 0; }
+}
 
 #define C_ASSERT_IMPL(actual_value, expected_value, comparator)\
     do {\
         if (not (actual_value comparator expected_value)) {\
-            throw "error";\
+	    fail<Str{"Assertion failed: " #actual_value " " #comparator " " #expected_value}>(); \
         }\
     } while (false)

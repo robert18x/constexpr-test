1ceb2c3ea3d577b981301634575c81069e15fa5c
fox18 <robertzurawski14@gmail.com>
2022-11-09 00:00:16 +0100

    Error message improvements

--- constexpr_test/constexpr_assert.h
+++ constexpr_test/constexpr_assert.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <string_view>
 
 #define C_ASSERT_EQ(actual_value, expected_value) C_ASSERT_IMPL(actual_value, expected_value, ==)
 #define C_ASSERT_NE(actual_value, expected_value) C_ASSERT_IMPL(actual_value, expected_value, !=)
@@ -13,24 +12,25 @@
 namespace constexpr_test {
     template <int length>
     struct constexpr_error_string {
-        char error_string[length];
-        constexpr constexpr_error_string(const char (&s)[length]) : error_string{} {
-            for (int i = 0; s[i]; ++i)
+        char error_string[length+1];
+        constexpr constexpr_error_string(const char (&s)[length]) : error_string() {
+            for (int i = 0; i < length; ++i) {
                 error_string[i] = s[i];
+            }
         }
     };
 
     template <auto length>
     constexpr_error_string(const char (&)[length]) -> constexpr_error_string<length>;
 
-    template <auto message>
-    constexpr void fail_compilation() { throw 0; }
+    template <auto>
+    constexpr void fail_compilation() { throw R"(Error - search for "CONSTEXPR_TEST_ERROR" in compiler output for more info)"; }
 }
 
 #define C_ASSERT_IMPL(actual_value, expected_value, comparator)\
     do\
     {\
         if (not (actual_value comparator expected_value)){\
-            fail_compilation<constexpr_error_string{"Assertion failed: " #actual_value " " #comparator " " #expected_value}>();\
+            fail_compilation<constexpr_error_string{"CONSTEXPR_TEST_ERROR - Assertion failed: " #actual_value " " #comparator " " #expected_value}>();\
         }\
     } while (false)
